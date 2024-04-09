f5b2bd0624eb5523277d056296f81222fb7424bc
Xtinaps <xtinasparakal@gmail.com>
2024-04-09 17:38:18 +0530

    search created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipie from './components/AddRecipie';
+import SearchRecipe from './components/SearchRecipe';
 
 function App() {
   return (
     <div>
      <AddRecipie/>
+     <SearchRecipe/>
     </div>
   );
 }

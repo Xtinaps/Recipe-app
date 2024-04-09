0f37366c1c1e02954af5a3489ee1226afee1c434
Xtinaps <xtinasparakal@gmail.com>
2024-04-09 18:40:07 +0530

    recipe view created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipie from './components/AddRecipie';
 import SearchRecipe from './components/SearchRecipe';
+import ViewRecipe from './components/ViewRecipe';
 
 function App() {
   return (
     <div>
      <AddRecipie/>
      <SearchRecipe/>
+     <ViewRecipe/>
     </div>
   );
 }
---
+++ src/components/ViewRecipe.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+const ViewRecipe = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img height= "300" src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://www.realsimple.com/thmb/c9fEoa5rbHfG4wE7fYr5lENfqQk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/skillet-polenta-in-tomato-sauce-recipe-BFY0222-12a0fcb5d726410aa1ba7f1e10168afe.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?quality=90&resize=556,505" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/prawn_fried_rice-2481925.jpg?quality=90&resize=440,400" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://hips.hearstapps.com/hmg-prod/images/easy-dinner-ideas-lasagna-soup-64529c039668f.jpg?crop=1.00xw:0.834xh;0,0.0965xh&resize=980:*" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://hips.hearstapps.com/hmg-prod/images/comfort-food-recipes-chicken-pasta-salad-646670ff9c384.jpeg?crop=0.8xw:1xh;center,top&resize=980:*" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://www.tasteofhome.com/wp-content/uploads/2018/01/Beef-with-Ramen-Noodles_EXPS_FT21_22747_F_1116_1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height= "300" src="https://cdn.loveandlemons.com/wp-content/uploads/2020/12/plant-based-recipes-1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewRecipe
